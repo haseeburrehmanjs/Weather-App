71d60c3283b2abef740d06ec6dbde951245e28b0
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-14 10:38:56 +0500

    modified work

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-let locat = document.querySelector('#location')
-let date = document.querySelector('#date')
-let temperature = document.querySelector('#temperature')
-let description = document.querySelector('#description')
-let img = document.querySelector('.Weather-Icon')
+// HTML ELEMENT USE IN JAVASCRIPT
 let input = document.querySelector('#input')
 let checkBtn = document.querySelector('#checkBtn')
 let form = document.querySelector('form')
+let div = document.querySelector('#main-div')
 
+// SUBMIT BUTTON
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-
     // API CALLING
     axios(`http://api.weatherapi.com/v1/current.json?key=e3e98122324b454b92f44333241406&q=${input.value}&aqi=no`)
         .then(responce => {
-            if (input.value == '') {
-                alert('Enter city Name')
-            } else {
-                let data = responce.data
-                console.log(data);
-                locat.innerHTML = data.location.name
-                date.innerHTML = data.location.localtime
-                temperature.innerHTML = data.current.
-                    temp_c
-                description.innerHTML = data.current.condition.text
-                img.src = data.current.condition.icon
-            }
+            let data = responce.data
+            // RENDER CARD
+            div.innerHTML += `
+        <div class="card mx-auto" style="width: 18rem;">
+        <div class="card-body text-center">
+          <h5 class="card-title" id="location">${data.location.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted" id="date">${data.location.localtime}</h6>
+          <img src="${data.current.condition.icon}" alt="Weather Icon" class="mb-2 Weather-Icon" id="weather-icon">
+          <h2 class="card-text" id="temperature">${data.current.temp_c}</h2>
+          <p class="card-text" id="description">${data.current.condition.text}</p>
+        </div>`
         })
         .catch(error => {
             console.log(error);
